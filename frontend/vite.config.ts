8b51587cf2570264835fc2984fe42d5411b43655
Dheerajydv <dy81035@gmail.com>
2025-06-18 22:03:44 +0530

    config.ts changed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:8000",
+                target: "https://mern-food-delivery-oz3t.onrender.com",
                 secure: false,
                 changeOrigin: true,
                 ws: true,
